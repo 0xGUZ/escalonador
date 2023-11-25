4e196d2fba2e9d418d104c0e839a7c8cded6f045
gustavo-villar-dev <gustavovm@dcc.ufrj.br>
2023-11-25 14:09:15 -0300

    alterei a struct PCB (tirei ppid)

--- headers/pcb.c
+++ headers/pcb.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 
 // Função para criar um novo PCB
-PCB createPCB(int pid, int priority, int cpu_time, int io_time) {
+PCB createPCB(int pid, int priority, int cpu_time, int io_time, int io_type) {
     PCB newPCB;
     newPCB.pid = pid;
+    newPCB.state = NEW; // Estado inicial é novo
     newPCB.priority = priority;
     newPCB.cpu_time = cpu_time;
     newPCB.io_time = io_time;
-    newPCB.state = NEW; // Estado inicial é novo
-    // Se criar novos campos mudar aqui
+    newPCB.io_type = io_type;
     return newPCB;
 }
 
@@ -22,8 +22,8 @@ PCB createRandomPCB() {
     int priority = (rand() % 5) + 1; // Prioridade entre 1 e 5
     int cpu_time = (rand() % 91) + 10; // Tempo de CPU entre 10 e 100
     int io_time = (rand() % 91) + 10; // Tempo de I/O entre 10 e 100
-
-    PCB randomPCB = createPCB(pid, priority, cpu_time, io_time);
+    int io_type = (rand() % 3);
+    PCB randomPCB = createPCB(pid, priority, cpu_time, io_time, io_type);
     pid++;
 
     return randomPCB;
