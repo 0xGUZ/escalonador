bb939d58b5eaed95f9d1d4abc5c713439eab913a
gustavo-villar-dev <gustavovm@dcc.ufrj.br>
2023-11-25 14:11:03 -0300

    usei as funções que eu codei

--- testing/queue-test.c
+++ testing/queue-test.c
@@ -5,36 +5,41 @@
 
 int main() {
     // Cria uma fila
-    Queue* myQueue = (Queue*)malloc(sizeof(Queue));
-    initializeQueue(myQueue);
+    Queue* fila1 = createQueue();
+    initializeQueue(fila1);
 
     // Adiciona alguns PCBs fictícios à fila
     for (int i = 0; i < 5; i++) {
         PCB pcb = createRandomPCB();
-        enqueue(myQueue, pcb);
+        enqueue(fila1, pcb);
     }
 
     // Imprime o conteúdo da fila
     printf("Conteudo da fila apos enfileirar PCBs:\n");
-    printQueue(myQueue);
+    printQueue(fila1);
 
     // Remove um PCB da fila e imprime seu PID
-    PCB removedPCB = dequeue(myQueue);
+    PCB removedPCB = dequeue(fila1);
     printf("\nPCB removido: PID = %d, Prioridade = %d\n", removedPCB.pid, removedPCB.priority);
 
     // Imprime o conteúdo da fila após a remoção
     printf("\nConteudo da fila apos desenfileirar um PCB:\n");
-    printQueue(myQueue);
+    printQueue(fila1);
 
     // Remove todos os PCBs restantes
-    dequeueAll(myQueue);
+    dequeueAll(fila1);
 
     // Verifica se a fila está vazia
     printf("\nConteudo da fila apos remover todos os PCBs:\n");
-    printQueue(myQueue);
+    printQueue(fila1);
+
+    printf("\nFila do arquivo: \n");
+    Queue* fila2 = queueFromFile("input.txt");
+    printQueue(fila2);
 
     // Libera a memória alocada para a fila
-    free(myQueue);
+    freeQueue(fila1);
+    freeQueue(fila2);
 
     return 0;
 }
